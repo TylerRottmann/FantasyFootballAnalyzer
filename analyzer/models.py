fbc377bb03e8b805f338b594912b1349e226479e
nwilfong12 <nwilfong@uwm.edu>
2026-09-21 22:24:44 -0500

    Adjusted User fields to build out league integration (#14)
    Co-authored-by: Nicholas Wilfong <madisonfc12@outlook.com>

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,5 +1,31 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    pass
+    email_notifications = models.BooleanField(
+        default=True
+    )
+
+    nickname = models.CharField(
+        max_length=150,
+        default="",
+    )
+
+    sleeper_username = models.CharField(
+        max_length=150,
+        default="",
+    )
+
+    yahoo_username = models.CharField(
+        max_length=150,
+        default="",
+    )
+
+    espn_username = models.CharField(
+        max_length=150,
+        default="",
+    )
+
+    def __str__(self):
+        return self.username
