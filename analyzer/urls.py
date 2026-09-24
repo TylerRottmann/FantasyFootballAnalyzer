76f73fa8910c16e2dab3be6f73a1ea06e4357e0b
Abhinav Reddy Donala <abhinavreddy@Abhinavs-MacBook-Air.local>
2026-09-24 11:32:27 -0500

    Add ADP rankings page for SCRUM-35

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,7 +1,8 @@
 # analyzer/urls.py
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, adp_rankings
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('adp/', adp_rankings, name='adp_rankings'),
 ]
